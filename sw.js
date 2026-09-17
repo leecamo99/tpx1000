752e9246626aa660b617a8d7b4007012b3253e5e
leecamo99 <130529345+leecamo99@users.noreply.github.com>
2026-09-17 14:17:43 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 /* ===== 金のフレーズ 單字 App - Service Worker ===== */
-var VER = 'kin-shell-v49';
+var VER = 'kin-shell-v50';
 var AUDIO = 'kin-audio-v1';
 var CORE = ['./', './index.html', './books.js', './manifest.webmanifest', './roadmap.html', './roadmap.json', './devtools.html'];
-
 self.addEventListener('install', function(ev){
   ev.waitUntil(caches.open(VER).then(function(c){
     return Promise.all(CORE.map(function(u){ return c.add(u).catch(function(){}); }));
