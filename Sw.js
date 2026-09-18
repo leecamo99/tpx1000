d11a2c41addd5e3d99245d871c78f7ea5f7e148b
leecamo99 <130529345+leecamo99@users.noreply.github.com>
2026-09-19 01:42:08 +0800

    Update service worker version to v58

--- Sw.js
+++ Sw.js
@@ -1,5 +1,5 @@
 /* ===== 金のフレーズ 單字 App - Service Worker ===== */
-var VER = 'kin-shell-v57';
+var VER = 'kin-shell-v58';
 var AUDIO = 'kin-audio-v1';
 var CORE = ['./', './index.html', './books.js', './manifest.webmanifest', './roadmap.html', './roadmap.json', './devtools.html'];
 
