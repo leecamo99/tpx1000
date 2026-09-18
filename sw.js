a5e139dd9b3c0f2a73e6aa61a8811ab23c36b7c8
leecamo99 <130529345+leecamo99@users.noreply.github.com>
2026-09-18 21:43:37 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ===== 金のフレーズ 單字 App - Service Worker ===== */
-var VER = 'kin-shell-v58-data-editor';
+var VER = 'kin-shell-v60-sentence-editor';
 var AUDIO = 'kin-audio-v1';
 var CORE = ['./', './index.html', './books.js', './shiko600.js', './books/kin1000.js', './manifest.webmanifest', './roadmap.html', './roadmap.json', './devtools.html'];
 
